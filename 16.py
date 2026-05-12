0ab16ccdf514ae34865a6468acc012c35c717ef6
mmermerid <cvetochek1480@gmail.com>
2026-05-12 23:28:41 +0300

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,13 @@
+while True:
+    from math import sqrt
+    print("Let's solve your quadratic equation!\n")
+    a = float(input("Your a is:\n"))
+    b = float(input("Your b is:\n"))
+    c = float(input("Your c is:\n"))
+    D = b**2-4*a*c
+    x1 = (-b + sqrt(D))/2*a
+    x2 = (-b - sqrt(D))/2*a
+    print (f"Your roots are: {x1} {x2}")
+    m = input("Do you have another quadratic equation?(y/n)\n")
+    if m == "n":
+        break
