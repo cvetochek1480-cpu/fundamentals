293ead2b48980449a50c7b407d0578bf9c992ea0
mmermerid <cvetochek1480@gmail.com>
2026-05-12 22:43:15 +0300

    h

---
+++ 14.py
@@ -0,0 +1,5 @@
+a = 6
+condition = a < 5
+print(condition)
+if condition:
+    print("a is less than 5")
---
+++ calculator.py
@@ -0,0 +1,18 @@
+while True:
+    number1 = int(input("Type the first number:\n"))
+    number2 = int(input("Type the second number:\n"))
+    operation = input("Type the operation (add, multiply, subtract, divide) or 'quit' to exit:\n")
+    if operation == "quit":
+        break
+    if operation == "add":
+        print("Result:", number1 + number2)
+    if operation == "multiply":
+        print("Result:",number1 * number2)
+    if operation == "subtract":
+        print("Result:",number1-number2)
+    if operation == "divide":
+        if number2 != 0:
+            print("Result:",number1/number2)
+        else:
+            print("Second number is 0. Can't divide by 0.")
+            
