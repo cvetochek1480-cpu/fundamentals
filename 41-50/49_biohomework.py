9829c2b0ac49fc42d93caf42f79c6bdab9813a51
mmermerid <cvetochek1480@gmail.com>
2026-06-10 02:17:17 +0300

    Update 49_biohomework.py

--- 41-50/49_biohomework.py
+++ 41-50/49_biohomework.py
@@ -1,18 +1,21 @@
 import csv #to use csv 
 #all of the inputs:
-liquid = input("Liquid Name:")
-month = input ("Arrival Month:")
-year = input ("Arrival Year:")
+def biology_inventory():
+    liquid = input("Liquid Name:")
+    month = input ("Arrival Month:")
+    year = input ("Arrival Year:")
 #opening a csv file in append mode and write in it and saving it:
-with open ('laboratory.csv', mode='a') as file:
-    writer = csv.writer(file)
-    writer.writerow ([liquid,month,year])
-print ("--- CURRENT LABORATORY INVENTORY ---") #decorations
+    with open ('laboratory.csv', mode='a') as file:
+        writer = csv.writer(file)
+        writer.writerow ([liquid,month,year])
+    print ("--- CURRENT LABORATORY INVENTORY ---") #decorations
 #opening csv and reading it:
-with open ('laboratory.csv', mode='r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        if not row: #if there is an empty line skip it
-            continue
-        print(f">{row[0]} arrived in {row[1]} {row[2]}.")
-print("-------------------------------------") #decorations
+    with open ('laboratory.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if not row: #if there is an empty line skip it
+                continue
+            print(f">{row[0]} arrived in {row[1]} {row[2]}.")
+    print("-------------------------------------") #decorations
+
+biology_inventory()
